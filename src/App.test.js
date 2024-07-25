a77a3409db49c3f3a4e93c6b265d191c42f652da
PravallikaKV <113497539+PravallikaKothaveerannagari@users.noreply.github.com>
2024-07-25 15:44:31 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App'; // Adjust the import path as per your project structure
+import { render, screen, act } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-
-  // Use a query selector to find the text case-insensitively
-  const linkElement = screen.getByText(/learn react/i);
-
+test('renders learn react link', async () => {
+  act(() => {
+    render(<App />);
+  });
+  const linkElement = await screen.findByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
